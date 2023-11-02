c4a317e3cc99383e8b28feb3aa1512cc1c62a029
Eren Baş <erenbas.info@gmail.com>
2023-11-02 23:39:38 +0300

    index fixed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+    getHello(): string {
+        return "<style>body { display: flex; justify-content: center; align-items: center; font-size: 40px; font-family: monospace; }</style>I'm still running";
+    }
+}
